a1f5e1e005b3d6fed987de62f948835f6a2dfffd
Ivan Kashuba <ikashuba@meninx.net>
2026-03-09 10:36:22 +0200

    paths changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/zeely_test_task/",
   server: {
     port: 3000,
   },
